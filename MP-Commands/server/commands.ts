8b3e99e3a6ac08cddf9cde764b2549c312403585
Joseph <joey.chm@comcast.net>
2024-04-25 09:42:59 -0700

    commands update

--- MP-Commands/server/commands.ts
+++ MP-Commands/server/commands.ts
@@ -1,32 +1,30 @@
 const MP = global.exports["MP-Base"].GetObject();
+const Delay = (ms: number) => new Promise(res => setTimeout(res, ms));
 
 export const commands = [
-    {
-        name: "ooc",
-        suggestion: "talk out of game character",
-        isAdmin: false,
-        handler: (source: number, args: string[]) => {
-            const msg = args.join(' ');
-            const Player = MP.Functions.GetPlayer(source);
-            const id = Player.Data.PlayerId;
-            const first = Player.Data.firstname;
-            const last = Player.Data.lastname;
-            const full = `|${id}| ${first} ${last}`;
-
+  {
+    name: "ooc",
+    suggestion: "talk out of game character",
+    isAdmin: false,
+    handler: (source: number, args: string[]) => {
+      const msg = args.join(' ');
+      const Player = MP.Functions.GetPlayer(source);
+      const id = Player.Data.PlayerId;
+      const first = Player.Data.firstname;
+      const last = Player.Data.lastname;
+      const full = `|${id}| ${first} ${last}`;
 			if (msg.length === 0) {
 				// dudes an idiot and forgot to type a msg LOL
 			} else {
 				emitNet('chat:addMessage', -1, `OOC: ${full}: ${msg}`);
 			}
-
-
-        }
-    },
+    }
+  },
 	{
-        name: "giveMoney",
-        suggestion: "Give money to player id, cash/bank, amount, add/del",
-        isAdmin: true,
-        handler: (source: number, args: string[]) => {
+    name: "giveMoney",
+    suggestion: "Give money to player id, cash/bank, amount, add/del",
+    isAdmin: true,
+    handler: (source: number, args: string[]) => {
 			const target = parseInt(args[0])// id
 			const Player = MP.Functions.GetPlayer(target)
 			const bankingType = (args[1]) // cash/bank
@@ -36,41 +34,37 @@ export const commands = [
 				global.exports['MP-Base'].changeMoney(target, bankingType, amount, changer)
 			} else  {
 				emitNet('MP-Elements:SendNotification', source,  2, "No Player Found." )
-
 			}
-        }
-    },
+    }
+  },
 	{
-        name: "saveCoords",
-        suggestion: "save coords for admins",
-        isAdmin: true,
-        handler: (source: number, args: string[]) => {
+    name: "saveCoords",
+    suggestion: "save coords for admins",
+    isAdmin: true,
+    handler: (source: number, args: string[]) => {
 			const src = source
 			const Player = MP.Functions.GetPlayer(src)
 			emitNet("chatMessage", -1, `CONSOLE: ${Player.Data.cid}`, 3);
 			emitNet("MP-Admin:Client:SaveCoords", source)
-        }
-    },
-
+    }
+  },
 	{
-        name: "console",
-        suggestion: "used for rcon",
-        isAdmin: true,
-        handler: (source: number, args: string[]) => {
-			const msg = args.join(' ');
-			emitNet("chatMessage", -1, `CONSOLE: ${msg}`, 3);
-        }
-    },
+    name: "console",
+    suggestion: "used for rcon",
+    isAdmin: true,
+    handler: (source: number, args: string[]) => {
+      const msg = args.join(' ');
+      emitNet("chatMessage", -1, `CONSOLE: ${msg}`, 3);
+    }
+  },
 	{
-        name: "setgroup",
-        suggestion: "used for rcon",
-        isAdmin: true,
-        handler: (source: number, args: string[]) => {
+    name: "setgroup",
+    suggestion: "used for rcon",
+    isAdmin: true,
+    handler: (source: number, args: string[]) => {
 			const target: number = parseInt(args[0]); // player id in game
 			const group = args[1]; // group [admin, mod, dev]
-
 			const player = MP.Functions.GetPlayer(target);
-
 			if (target !== null) {
 				if (player) {
 					if (MP.UserGroups[group]) {
@@ -87,16 +81,14 @@ export const commands = [
 		}
 	},
 	{
-        name: "addMoney",
-        suggestion: "adds money to player id, cash/bank, amount",
-        isAdmin: true,
-        handler: (source: number, args: string[]) => {
+    name: "addMoney",
+    suggestion: "adds money to player id, cash/bank, amount",
+    isAdmin: true,
+    handler: (source: number, args: string[]) => {
 			const target: number = parseInt(args[0]); // player id in game
 			const type = args[1]; // cash or bank
 			const amount = parseInt(args[2]); // amount
-
 			const player = MP.Functions.GetPlayer(target);
-
 			if (target !== null) {
 				if (player) {
 					if (type && amount) {
@@ -111,6 +103,56 @@ export const commands = [
 				}
 			}
 		}
-    },
-    // Other commands
+  },
+	// Used to set player's job
+	{
+    name: "setjob",
+    suggestion: "setjob player id, job id, job grade",
+    isAdmin: true,
+    handler: async (source: number, args: string[]) => {
+      const target = parseInt(args[0])// id
+      const Player = MP.Functions.GetPlayer(target)
+      const curJob = global.exports['MP-Base'].GetPlayerData(source, 'job');
+      const job = (args[1]) // job name
+      const grade = parseInt(args[2])  // grade
+      if (Player !== null)  {
+        emitNet(curJob+':client:unloadJob', target) // unloads the previous job
+        console.log(Player.Data.job+':client:unloadJob', target)
+        emit(job+':server:changeJob', target, job, grade) // changes job to new job
+        await Delay(500)
+        emitNet(job+':client:loadJob', target) // loads the new job
+      } else {
+        emitNet('MP-Elements:SendNotification', source,  2, "No Player Found." )
+      }
+    }
+  },
+	{
+    name: "job",
+    suggestion: "View your current occupation",
+    isAdmin: false,
+    handler: (source: number, args: string[]) => {
+			const Player = MP.Functions.GetPlayer(source)
+			if (Player !== null)  {
+				emit('server:getJob', source)
+			} else  {
+				emitNet('MP-Elements:SendNotification', source,  2, "Something is wrong, contact administrator." )
+			}
+    }
+  },
+	{
+    name: "adminmenu",
+    suggestion: "(admin only) opens admin menu",
+    isAdmin: true,
+    handler: (source: number, args: string[]) => {
+      const target = parseInt(args[0])// id
+      const Player = MP.Functions.GetPlayer(target)
+      const job = (args[1]) // job name
+      const grade = parseInt(args[2])  // grade
+      if (source !== null)  {
+        emitNet('admin:client:openAdminMenu', source)
+      } else  {
+        emitNet('MP-Elements:SendNotification', source,  2, "Something is wrong here..." )
+      }
+    }
+  },
 ];
