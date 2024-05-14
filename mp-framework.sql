d68edb30450f935860242619953274a8cd47f765
Joseph <joey.chm@comcast.net>
2024-05-14 16:02:58 -0700

    Update mp-framework.sql

--- mp-framework.sql
+++ mp-framework.sql
@@ -4,11 +4,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-
---
--- Database: `mp-framework`
---
 CREATE DATABASE IF NOT EXISTS `mp-framework` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `mp-framework`;
 
